8e5a25140f310e15c1ff0e00f886c650c11a0f92
Ron <rc72095@icloud.com>
2023-01-08 14:07:57 -0500

    add get endpoint for profile/:id

--- SmartBrainAPI/server.js
+++ SmartBrainAPI/server.js
@@ -50,6 +50,20 @@ app.post('/register', (req, res) => {
   res.json(database.users[database.users.length-1]);
 })
 
+app.get('/profile/:id', (req, res) => {
+  const {id} = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if(user.id === id){
+      found = true;
+      return res.json(user);
+    }
+    if(!found) {
+      return res.status(400).json('error logging in');
+     }
+  })
+})
+
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
