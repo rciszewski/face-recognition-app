1bceec05610f6ab6c4292f391b8d44ac79d832d2
Ron <rc72095@icloud.com>
2023-01-10 19:23:18 -0500

    add bcrypt for security

--- SmartBrainAPI/server.js
+++ SmartBrainAPI/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 app.use(express.json());
@@ -21,6 +22,13 @@ const database = {
       entries: 0,
       joined: new Date()
     }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com'
+    }
   ]
 }
 
@@ -39,6 +47,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const {name, email, password} = req.body;
+  bcrypt.hash(password, null, null, function(err, hash) {
+    console.log(hash)
+  });
   database.users.push({
     id: '125',
     name: name,
@@ -76,6 +87,18 @@ app.put('/image', (req, res) => {
   })
   if(!found) return res.send(400).json('error updating entries')
 })
+
+bcrypt.hash("bacon", null, null, function(err, hash) {
+  // Store hash in your password DB.
+});
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//   // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//   // res = false
+// });
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
