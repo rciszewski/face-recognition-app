c5c158d4e47484bcc9a95680e5e3329ab405ce61
Ron <rc72095@icloud.com>
2023-01-16 09:48:38 -0500

    add cors npm package for origin access control

--- SmartBrainAPI/server.js
+++ SmartBrainAPI/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const database = {
   users: [
