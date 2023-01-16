045830435001856e9c0beefdc6d5ec432e74f20f
Ron <rc72095@icloud.com>
2023-01-16 16:20:04 -0500

    check to determine if the user object returned has an id property

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -29,7 +29,7 @@ class SignIn extends Component {
     })
       .then(response => response.json())
       .then(user => {
-        if(user){
+        if(user.id){
           this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
