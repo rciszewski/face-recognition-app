c9fc448508722f7de8634f3818972ab4d5cb2fb3
Ron <rc72095@icloud.com>
2023-02-01 18:42:29 -0500

    signin w/ verifying email and password

--- SmartBrainAPI/server.js
+++ SmartBrainAPI/server.js
@@ -19,46 +19,32 @@ app.use(express.json());
 app.use(cors());
 
 
-
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'John@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'Sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'john@gmail.com'
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
-  res.send(database.users);
+  db.select('*').from('users')
+    .then(users => {
+      res.send(users);
+    })
+    .catch(err => res.status(400).json('unable to get users'))
 })
 
 app.post('/signin', (req, res) => {
-  if(req.body.email === database.users[0].email &&
-     req.body.password === database.users[0].password){
-    res.json(database.users[0]);
-     } else {
-      res.status(400).json('error logging in');
-     }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if(isValid){
+       return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('wrong credentials'))
+      }
+      else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
@@ -82,24 +68,6 @@ app.post('/register', (req, res) => {
             res.json(user[0])
           })
       })
-
-    //   trx.insert({hash: hash, email: email})
-    //   .into('login')
-    //   .returning('email')
-    //   .then(loginEmail => {
-    //     return trx('users')
-    //     .returning('*')
-    //     .insert({
-    //       email: loginEmail[0].email,
-    //       name: name,
-    //       joined: new Date()
-    //     })
-    //   .then(user => {
-    //     res.json(user[0]);
-    //   })
-    // })
-    // .then(trx.commit)
-    // .catch(trx.rollback)
   })
   .catch(err => res.status(400).json('unable to register'))
 })
@@ -129,29 +97,6 @@ app.put('/image', (req, res) => {
   .catch(err => res.status(400).json('error updating entries'))
 })
 
-// bcrypt.hash("bacon", null, null, function(err, hash) {
-//   // Store hash in your password DB.
-// });
-
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//   // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//   // res = false
-// });
-
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
-
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = new user
-/profile/:userId --> GET = user
-/image --> PUT = user count
-
-
-
-*/
